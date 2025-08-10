2860c999522ae6c877f73c95342553505fe61f2a
liwkss <34839664+liwkss@users.noreply.github.com>
2025-08-11 00:37:02 +0800

    Add files via upload

--- flux_fp8.sh
+++ flux_fp8.sh
@@ -27,7 +27,7 @@ WORKFLOWS=(
 CLIP_MODELS=(
     "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors"
     #"https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp16.safetensors"
-	"https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp8_e4m3fn_scaled.safetensors"
+    "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp8_e4m3fn_scaled.safetensors"
 )
 
 UNET_MODELS=(
